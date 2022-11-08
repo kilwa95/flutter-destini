827088b1ba9127ddde61248a10f37bc8a45c9e5c
khaled <lga.sig.khaled@gmail.com>
2022-11-08 17:12:36 +0100

    commit final

---
+++ lib/story.dart
@@ -0,0 +1,10 @@
+class Story {
+  Story({
+    required this.storyTitle,
+    required this.choice1,
+    required this.choice2,
+  });
+  late String storyTitle;
+  late String choice1;
+  late String choice2;
+}
